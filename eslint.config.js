d3322c4335e4993a28322b0f08d70291455c6bed
CletsyMedia <Cletussam12@yahoo.com>
2024-06-16 08:06:13 +0100

    Newest Update

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,44 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 import { fixupConfigRules } from "@eslint/compat";
 
-
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  { files: ["**/*.jsx"], languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  ...fixupConfigRules(pluginReactConfig),
-];
+export default {
+  overrides: [
+    {
+      files: ["**/*.js"],
+      extends: [
+        "eslint:recommended",
+        "plugin:@eslint/recommended-recommended",
+      ],
+      parserOptions: {
+        ecmaVersion: 2023,
+      },
+    },
+    {
+      files: ["**/*.jsx"],
+      extends: [
+        "eslint:recommended",
+        "plugin:@eslint/recommended-recommended",
+        "plugin:react/recommended",
+      ],
+      parserOptions: {
+        ecmaVersion: 2023,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      settings: {
+        react: {
+          version: "detect",
+        },
+      },
+      rules: {
+        "react/prop-types": "off",
+        "react/react-in-jsx-scope": "off",
+        "no-unused-vars": "warn",
+      },
+    },
+  ],
+  globals: globals.browser,
+};
